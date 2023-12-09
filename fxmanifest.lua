83814465de140e9e6a708bc0b6ee6b2444e2b434
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-12-08 19:16:43 -0500

    refactor: switch to core modules

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,21 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'QB-TruckRobbery'
+description 'qbx_truckrobbery'
+repository 'https://github.com/Qbox-project/qbx_truckrobbery'
 version '1.0.0'
 
+shared_scripts {
+    '@ox_lib/init.lua',
+    '@qbx_core/modules/utils.lua',
+}
+
+client_scripts {
+    '@qbx_core/modules/playerdata.lua',
+    'client/main.lua',
+}
+
 server_script 'server/main.lua'
-client_script 'client/main.lua'
 
 lua54 'yes'
+use_experimental_fxv2_oal 'yes'
--- server/main.lua
+++ server/main.lua
@@ -1,44 +1,44 @@
-local QBCore = exports['qbx-core']:GetCoreObject()
 local ActivePolice = 2  		--<< needed policemen to activate the mission
 local cashA = 250 				--<<how much minimum you can get from a robbery
 local cashB = 450				--<< how much maximum you can get from a robbery
 local ActivationCost = 500		--<< how much is the activation of the mission (clean from the bank)
 local ResetTimer = 2700 * 1000  --<< timer every how many missions you can do, default is 600 seconds
 local ActiveMission = 0
+local ITEMS = exports.ox_inventory:Items()
 
 RegisterServerEvent('AttackTransport:akceptujto', function()
 	local copsOnDuty = 0
 	local _source = source
-	local xPlayer = QBCore.Functions.GetPlayer(_source)
-	local accountMoney = xPlayer.PlayerData.money["bank"]
+	local xPlayer = exports.qbx_core:GetPlayer(_source)
+	local accountMoney = xPlayer.PlayerData.money['bank']
 	if ActiveMission == 0 then
 		if accountMoney < ActivationCost then
-		TriggerClientEvent('QBCore:Notify', _source, "You need $"..ActivationCost.." in the bank to accept the mission")
+			exports.qbx_core:Notify( _source, 'You need $'..ActivationCost..' in the bank to accept the mission')
 		else
-			for _, v in pairs(QBCore.Functions.GetPlayers()) do
-				local Player = QBCore.Functions.GetPlayer(v)
+			for _, v in pairs(exports.qbx_core:GetPlayers()) do
+				local Player = exports.qbx_core:GetPlayer(v)
 				if Player ~= nil then
-					if (Player.PlayerData.job.name == "police" and Player.PlayerData.job.onduty) then
+					if (Player.PlayerData.job.name == 'police' and Player.PlayerData.job.onduty) then
 						copsOnDuty = copsOnDuty + 1
 					end
 				end
 			end
 			if copsOnDuty >= ActivePolice then
-				TriggerClientEvent("AttackTransport:Pozwolwykonac", _source)
-				xPlayer.Functions.RemoveMoney('bank', ActivationCost, "armored-truck")
+				TriggerClientEvent('AttackTransport:Pozwolwykonac', _source)
+				xPlayer.Functions.RemoveMoney('bank', ActivationCost, 'armored-truck')
 
 				OdpalTimer()
 			else
-				TriggerClientEvent('QBCore:Notify', _source, 'Need at least '..ActivePolice.. ' SASP to activate the mission.')
+				exports.qbx_core:Notify(_source, 'Need at least '..ActivePolice.. ' SASP to activate the mission.')
 			end
 		end
 	else
-		TriggerClientEvent('QBCore:Notify', _source, 'Someone is already carrying out this mission')
+		exports.qbx_core:Notify(_source, 'Someone is already carrying out this mission')
 	end
 end)
 
 RegisterServerEvent('qb-armoredtruckheist:server:callCops', function(streetLabel, coords)
-    TriggerClientEvent("qb-armoredtruckheist:client:robberyCall", -1, streetLabel, coords)
+    TriggerClientEvent('qb-armoredtruckheist:client:robberyCall', -1, streetLabel, coords)
 end)
 
 function OdpalTimer()
@@ -54,20 +54,20 @@ end)
 
 RegisterServerEvent('AttackTransport:graczZrobilnapad', function()
 	local _source = source
-	local xPlayer = QBCore.Functions.GetPlayer(_source)
+	local xPlayer = exports.qbx_core:GetPlayer(_source)
 	local bags = math.random(1,3)
 	local info = {
 		worth = math.random(cashA, cashB)
 	}
 	xPlayer.Functions.AddItem('markedbills', bags, false, info)
-	TriggerClientEvent('inventory:client:ItemBox', _source, QBCore.Shared.Items['markedbills'], "add")
+	TriggerClientEvent('inventory:client:ItemBox', _source, ITEMS['markedbills'], 'add')
 
 	local chance = math.random(1, 100)
-	TriggerClientEvent('QBCore:Notify', _source, 'You took '..bags..' bags of cash from the van')
+	exports.qbx_core:Notify(_source, 'You took '..bags..' bags of cash from the van')
 
 	if chance >= 95 then
 		xPlayer.Functions.AddItem('security_card_01', 1)
-		TriggerClientEvent('inventory:client:ItemBox', _source, QBCore.Shared.Items['security_card_01'], "add")
+		TriggerClientEvent('inventory:client:ItemBox', _source, ITEMS['security_card_01'], 'add')
 	end
 	Wait(2500)
 end)
